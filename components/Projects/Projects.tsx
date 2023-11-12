de3e8af1c90e0dc82a3811a9f148ad0b129b875c
ViraMesha <wira.mescha@gmail.com>
2023-11-12 19:47:06 +0200

    feat: Change initial value of pageCount

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -13,13 +13,15 @@ type ProjectsProps = {
 
 const Projects = ({ className, itemsPerPage }: ProjectsProps) => {
   const [currentItems, setCurrentItems] = useState<Project[] | null>(null);
-  const [pageCount, setPageCount] = useState(0);
+  const [pageCount, setPageCount] = useState(1);
   const [itemOffset, setItemOffset] = useState(0);
   const [clickPaginate, setClickPaginate] = useState(false);
 
   const items = allProjects.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date))
   );
+
+  console.log(pageCount);
 
   const ref = useRef<HTMLDivElement>(null);
 
